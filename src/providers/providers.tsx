500f46084fd8bec8a0effbf8448fd96bfb0dd495
Gustavo Hariel <78812552+gustavohariel@users.noreply.github.com>
2024-06-22 10:48:55 -0300

    providers config, react query and theme provider

---
+++ src/providers/providers.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from './theme-provider';
+import { ReactNode } from 'react';
+
+const queryClient = new QueryClient();
+
+export const Providers = ({ children }: { children: ReactNode }) => (
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      {children}
+    </ThemeProvider>
+    {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+  </QueryClientProvider>
+);
