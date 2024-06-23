e94c5bed0980cce02e6fe08e970b32cc1e69eac0
Gustavo Hariel <78812552+gustavohariel@users.noreply.github.com>
2024-06-23 04:30:52 -0300

    add jokes card

--- src/components/jokes-card.tsx
+++ src/components/jokes-card.tsx
@@ -1,86 +1,69 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 import { ScrollArea } from './ui/scroll-area';
 import ChuckAvatar from './chuck-avatar';
-import { useGetRandomJoke } from '@/lib/data/get-random-joke';
-import { useGetSearchJoke } from '@/lib/data/get-search-joke';
+import { useJoke } from '@/contexts/joke-context';
 import { Joke } from '@/lib/types';
+import { cn } from '@/lib/utils';
+import { LoaderPinwheelIcon } from 'lucide-react';
+import { useToast } from './ui/use-toast';
 
 export default function JokesCard() {
-  const {
-    data: randomJoke,
-    error: randomError,
-    isPending: randomIsLoading,
-    dataUpdatedAt: randomUpdated,
-  } = useGetRandomJoke();
-  const {
-    data: searchJoke,
-    error: searchError,
-    isPending: searchIsLoading,
-    dataUpdatedAt: searchUpdated,
-  } = useGetSearchJoke();
+  const { joke, isLoading, error } = useJoke();
+  const { toast } = useToast();
 
-  const isLoading = randomIsLoading || searchIsLoading;
-
-  const error = randomError || searchError;
-
-  let jokeToRender;
-
-  if (randomUpdated > searchUpdated) {
-    jokeToRender = [randomJoke];
-  }
-
-  if (searchUpdated > randomUpdated) {
-    jokeToRender = searchJoke.result;
-  }
-
-  // jokeToRender =
-  //   randomJoke !== undefined
-  //     ? [randomJoke]
-  //     : searchJoke && searchJoke.result.length > 0
-  //       ? searchJoke.result
-  //       : null;
+  useEffect(() => {
+    if (error) {
+      toast({
+        title: 'Chuck Norris can compile syntax errors.',
+        description: 'We had an server error, please try again.',
+      });
+    }
+  }, [error, toast]);
 
   return (
     <>
-      {error && <p>Error fetching joke: {error.message}</p>}
-      {jokeToRender && (
-        <Card className="w-auto min-h-52">
+      {joke && (
+        <Card className="w-auto max-w-[450px]">
           <CardHeader>
-            <CardTitle>Joke Result</CardTitle>
+            <CardTitle className="flex items-center">
+              Joke Result
+              {isLoading && (
+                <span className="ml-auto">
+                  <LoaderPinwheelIcon className="h-5 w-5 animate-spin" />
+                </span>
+              )}
+            </CardTitle>
           </CardHeader>
           <CardContent>
             <ScrollArea className="h-[200px]">
               <div className="flex h-full flex-col">
-                <div className="flex flex-1 flex-col">
-                  <div>
-                    {isLoading && <p>Loading...</p>}
-                    <div className="text-sm">
-                      {jokeToRender.map((joke: Joke) => (
-                        <div
-                          key={joke.id}
-                          className="flex items-start gap-4 mt-2"
-                        >
-                          <div className="flex flex-col">
-                            <ChuckAvatar />
-                            <span className="mt-2 text-foreground">
-                              Chuck Norris
-                            </span>
-                          </div>
-                          <div className="grid gap-1">
-                            <div className="group gap-1 font-semibold"></div>
-                            <div className="word-break flex-1 whitespace-pre-wrap text-sm">
-                              <blockquote className="border-l-2 pl-6 italic text-wrap">
-                                {joke.value}
-                              </blockquote>
-                            </div>
-                          </div>
+                <div className="text-sm">
+                  {joke.result.map((joke: Joke) => (
+                    <div
+                      key={joke.id}
+                      className={cn('flex items-start gap-4 mt-2', {
+                        'brightness-50': isLoading,
+                      })}
+                    >
+                      <div className="flex flex-col">
+                        <ChuckAvatar />
+                        <span className="mt-2 text-foreground">
+                          Chuck Norris
+                        </span>
+                      </div>
+                      <div className="grid gap-1">
+                        <div className="group gap-1 font-semibold"></div>
+                        <div className="word-break flex-1 whitespace-pre-wrap text-sm">
+                          <blockquote className="border-l-2 pl-6 italic text-wrap">
+                            {joke.value}
+                          </blockquote>
                         </div>
-                      ))}
+                      </div>
                     </div>
-                  </div>
+                  ))}
                 </div>
               </div>
             </ScrollArea>
