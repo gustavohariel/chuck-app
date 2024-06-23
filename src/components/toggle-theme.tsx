a6c568b4915289a53bf95ae1c71749ca43d61db8
Gustavo Hariel <78812552+gustavohariel@users.noreply.github.com>
2024-06-23 04:29:44 -0300

    add hell theme

--- src/components/toggle-theme.tsx
+++ src/components/toggle-theme.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import * as React from 'react';
-import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import { useTheme } from 'next-themes';
 import { Flame } from 'lucide-react';
 import { Button } from '@/components/ui/button';
@@ -19,20 +18,17 @@ export function ToggleTheme() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="sm" className="h-6 w-6 px-0">
-          <SunIcon className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Flame className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Flame className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onSelect={() => setTheme('light')}>
-          Light
+          Hell
         </DropdownMenuItem>
         <DropdownMenuItem onSelect={() => setTheme('dark')}>
           Hell
-        </DropdownMenuItem>
-        <DropdownMenuItem onSelect={() => setTheme('system')}>
-          System
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
