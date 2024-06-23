2d5b2effcc542c6f4966666d75c71e910bbc6c2d
Gustavo Hariel <78812552+gustavohariel@users.noreply.github.com>
2024-06-23 03:35:50 -0300

    remove refetch on focus

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -5,7 +5,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from './theme-provider';
 import { ReactNode } from 'react';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export const Providers = ({ children }: { children: ReactNode }) => (
   <QueryClientProvider client={queryClient}>
